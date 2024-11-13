d16f8d408d6eaee2171d3c7917e7d86b00b37434
vp278 <vp278@cornell.edu>
2024-11-13 16:25:34 -0500

    Alt works for hits, need to implement LRU [skip ci]

--- sim/lab3_mem/CacheAltCtrl.v
+++ sim/lab3_mem/CacheAltCtrl.v
@@ -5,9 +5,9 @@
 `ifndef LAB3_MEM_CACHE_ALT_CTRL_V
 `define LAB3_MEM_CACHE_ALT_CTRL_V
 
-//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-// LAB TASK: Include necessary files
-//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+`include "vc/regfiles.v"
+`include "vc/regs.v"
+`include "vc/mem-msgs.v"
 
 module lab3_mem_CacheAltCtrl
 #(
@@ -19,23 +19,44 @@ module lab3_mem_CacheAltCtrl
 
   // Processor <-> Cache Interface
 
-  input  logic        proc2cache_reqstream_val,
-  output logic        proc2cache_reqstream_rdy,
+  input  logic        cachereq_val,
+  output logic        cachereq_rdy,
 
-  output logic        proc2cache_respstream_val,
-  input  logic        proc2cache_respstream_rdy,
+  output logic        cacheresp_val,
+  input  logic        cacheresp_rdy,
 
   // Cache <-> Memory Interface
 
-  output logic        cache2mem_reqstream_val,
-  input  logic        cache2mem_reqstream_rdy,
+  output logic        memreq_val,
+  input  logic        memreq_rdy,
 
-  input  logic        cache2mem_respstream_val,
-  output logic        cache2mem_respstream_rdy,
+  input  logic        memresp_val,
+  output logic        memresp_rdy,
 
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // LAB TASK: Define additional ports
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // control signals (ctrl->dpath)
+
+  output logic        cachereq_reg_en,
+  output logic        tag_array_wen,
+  output logic        tag_array_ren,
+  output logic        data_array_wen,
+  output logic        data_array_ren,
+
+  output logic        memresp_en,
+  output logic        write_data_mux_sel,
+  output logic        wben_mux_sel,
+  output logic        read_data_zero_mux_sel,
+  output logic        memreq_addr_mux_sel,
+  output logic        read_data_reg_en,
+  output logic        evict_addr_reg_en,
+  output logic [3:0]  cacheresp_type,
+  output logic        hit,
+  output logic [3:0]  memreq_type,
+
+  // status signals (dpath->ctrl)
+
+  input  logic  [3:0] cachereq_type,
+  input  logic [31:0] cachereq_addr,
+  input  logic        tag_match
 );
 
   //----------------------------------------------------------------------
@@ -55,9 +76,319 @@ module lab3_mem_CacheAltCtrl
   localparam STATE_EVICT_WAIT        = 5'd10;
   localparam STATE_WAIT              = 5'd11;
 
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // LAB TASK: Impement control unit
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  //----------------------------------------------------------------------
+  // State
+  //----------------------------------------------------------------------
+
+  always @( posedge clk ) begin
+    if ( reset ) begin
+      state_reg <= STATE_IDLE;
+    end
+    else begin
+      state_reg <= next_state;
+    end
+  end
+
+  //----------------------------------------------------------------------
+  // State Transitions
+  //----------------------------------------------------------------------
+
+  logic is_read;
+  logic is_write;
+  logic is_init;
+ 
+  logic way0_is_valid;
+  logic way1_is_valid;
+  logic [1:0] hit_TC;
+  assign hit_TC = {(way0_is_valid && tag_match) , (way1_is_valid && tag_match)};
+
+  assign is_read  = (cachereq_type == `VC_MEM_REQ_MSG_TYPE_READ);
+  assign is_write = (cachereq_type == `VC_MEM_REQ_MSG_TYPE_WRITE);
+  assign is_init  = (cachereq_type == `VC_MEM_REQ_MSG_TYPE_WRITE_INIT);
+
+  logic [4:0] state_reg;
+  logic [4:0] next_state;
+
+  logic way0_is_dirty;
+  logic way1_is_dirty;
+  logic is_dirty;
+  assign is_dirty = (way0_is_dirty || way1_is_dirty);
+
+
+  always @(*) begin
+
+    case ( state_reg )
+
+      STATE_IDLE: begin
+        // Waits for a cache request and then transitions to STATE_TAG_CHECK if a request is valid.
+        if ( cachereq_val ) next_state = STATE_TAG_CHECK;
+        else                next_state = STATE_IDLE;
+      end
+
+      STATE_TAG_CHECK: begin
+        if ( is_init )                                                                   next_state = STATE_INIT_DATA_ACCESS;
+        else if ( is_read && hit_TC )     next_state = STATE_READ_DATA_ACCESS;  //Read hit
+        else if ( is_write && hit_TC )    next_state = STATE_WRITE_DATA_ACCESS; //Write hit
+        else if ( !hit_TC && is_dirty  )  next_state = STATE_EVICT_PREPARE;     //Write miss when cache line is dirty
+        else if ( !hit_TC && !is_dirty  ) next_state = STATE_REFILL_REQUEST;    //Read miss or Write miss when cache line is NOT dirty
+        else                                                                             next_state = STATE_IDLE;
+      end
+
+      STATE_INIT_DATA_ACCESS: begin
+        next_state = STATE_WAIT;
+      end
+
+      STATE_READ_DATA_ACCESS: begin
+        next_state = STATE_WAIT;
+      end
+
+      STATE_WRITE_DATA_ACCESS: begin
+        next_state = STATE_WAIT;
+      end
+
+      STATE_REFILL_REQUEST: begin
+        if (memreq_rdy) next_state = STATE_REFILL_WAIT;
+        else            next_state = STATE_REFILL_REQUEST;
+      end
+
+      STATE_REFILL_WAIT: begin
+        if (memresp_val)  next_state = STATE_REFILL_UPDATE;
+        else              next_state = STATE_REFILL_WAIT;
+      end
+
+      STATE_REFILL_UPDATE: begin
+        if (is_write)     next_state = STATE_WRITE_DATA_ACCESS;
+        else if (is_read) next_state = STATE_READ_DATA_ACCESS;
+        else              next_state = STATE_IDLE;
+      end
+
+      STATE_EVICT_PREPARE: begin
+        next_state = STATE_EVICT_REQUEST;
+      end
+
+      STATE_EVICT_REQUEST: begin
+        if (memreq_rdy) next_state = STATE_EVICT_WAIT;
+        else            next_state = STATE_EVICT_REQUEST;
+      end
+
+      STATE_EVICT_WAIT: begin
+        if (memresp_val)  next_state = STATE_REFILL_REQUEST;
+        else              next_state = STATE_EVICT_WAIT;
+      end
+
+      STATE_WAIT: begin
+        if (cacheresp_rdy)  next_state = STATE_IDLE;
+        else                next_state = STATE_WAIT;
+      end
+
+      default: begin
+        next_state = STATE_IDLE;
+      end
+
+    endcase
+
+  end
+
+  //----------------------------------------------------------------------
+  // Valid/Dirty bits record
+  // Store valid and dirty bits for each cache entry / set
+  //----------------------------------------------------------------------
+
+  logic [2:0] cachereq_addr_index;
+
+  always @(*) begin
+    if ( p_num_banks == 1 ) begin
+      cachereq_addr_index = cachereq_addr[6:4];
+    end
+    else if ( p_num_banks == 4 ) begin
+      cachereq_addr_index = cachereq_addr[8:6];
+    end
+  end
+
+  logic valid_bits_write_en;
+  logic dirty_bits_write_en;
+  logic valid_bit_in;
+  logic dirty_bit_in;
+
+  vc_ResetRegfile_1r1w
+  #(
+    .p_data_nbits (1),
+    .p_num_entries(8),
+    .p_reset_value(0)     // On reset, all values are not valid.
+  ) valid_bit_way0
+  (
+    .clk        (clk),
+    .reset      (reset),
+    .read_addr  (cachereq_addr_index),
+    .read_data  (way0_is_valid),
+    .write_en   (valid_bits_write_en),
+    .write_addr (cachereq_addr_index),
+    .write_data (valid_bit_in)
+  );
+
+  vc_ResetRegfile_1r1w
+  #(
+    .p_data_nbits (1),
+    .p_num_entries(8),
+    .p_reset_value(0)     // On reset, all values are not valid.
+  ) valid_bit_way1
+  (
+    .clk        (clk),
+    .reset      (reset),
+    .read_addr  (cachereq_addr_index),
+    .read_data  (way1_is_valid),
+    .write_en   (valid_bits_write_en),
+    .write_addr (cachereq_addr_index),
+    .write_data (valid_bit_in)
+  );
+
+    vc_ResetRegfile_1r1w
+  #(
+    .p_data_nbits (1),
+    .p_num_entries(8),
+    .p_reset_value(0)     // On reset, all values are not dirty.
+  ) dirty_bits_way0
+  (
+    .clk        (clk),
+    .reset      (reset),
+    .read_addr  (cachereq_addr_index),
+    .read_data  (way0_is_dirty),
+    .write_en   (dirty_bits_write_en),
+    .write_addr (cachereq_addr_index),
+    .write_data (dirty_bit_in)
+  );
+
+    vc_ResetRegfile_1r1w
+  #(
+    .p_data_nbits (1),
+    .p_num_entries(8),
+    .p_reset_value(0)     // On reset, all values are not dirty.
+  ) dirty_bits_way1
+  (
+    .clk        (clk),
+    .reset      (reset),
+    .read_addr  (cachereq_addr_index),
+    .read_data  (way1_is_dirty),
+    .write_en   (dirty_bits_write_en),
+    .write_addr (cachereq_addr_index),
+    .write_data (dirty_bit_in)
+  );
+
+  //----------------------------------------------------------------------
+  // Store cache_resp_type and hit in a enable_register
+  //----------------------------------------------------------------------
+  logic       cacheresp_type_wen;
+  logic       hit_wen;
+  logic [3:0] cacheresp_type_in;
+
+  vc_EnResetReg
+  #(
+    .p_nbits(2),
+    .p_reset_value(0)
+  ) hit_reg
+  (
+    .clk(clk),
+    .reset(reset),
+    .d(cachereq_type == `VC_MEM_REQ_MSG_TYPE_WRITE_INIT ? 2'b0 : hit_TC),
+    .q(hit),
+    .en(hit_wen)
+  );
+
+  vc_EnResetReg
+  #(
+    .p_nbits(4),
+    .p_reset_value(`VC_MEM_RESP_MSG_TYPE_X)
+  ) cacheresp_type_reg
+  (
+    .clk(clk),
+    .reset(reset),
+    .d(cacheresp_type_in),
+    .q(cacheresp_type),
+    .en(cacheresp_type_wen)
+  );
+
+  //----------------------------------------------------------------------
+  // State Outputs
+  //----------------------------------------------------------------------
+
+  task cs
+  (
+    input logic         cs_cachereq_rdy,
+    input logic         cs_cacheresp_val,
+    input logic         cs_cachereq_reg_en,
+    input logic         cs_tag_array_wen,
+    input logic         cs_tag_array_ren,
+    input logic         cs_data_array_wen,
+    input logic         cs_data_array_ren,
+    input logic         cs_valid_bit_in,
+    input logic         cs_valid_bits_write_en,
+    input logic         cs_dirty_bit_in,
+    input logic         cs_dirty_bits_write_en,
+    input logic         cs_memreq_val,
+    input logic         cs_memresp_rdy,
+    input logic         cs_memresp_en,
+    input logic         cs_write_data_mux_sel,
+    input logic         cs_wb_mux_sel,
+    input logic         cs_read_data_zero_mux_sel,
+    input logic         cs_memreq_addr_mux_sel,
+    input logic         cs_read_data_reg_en,
+    input logic         cs_evict_addr_reg_en,
+    input logic         cs_hit_wen,
+    input logic         cs_cacheresp_type_wen,     // update registered value of hit and cacheresp_type on HIGH
+    input logic [3:0]   cs_cacheresp_type,
+    input logic [3:0]   cs_memreq_type
+  );
+  begin
+    cachereq_rdy            = cs_cachereq_rdy;
+    cacheresp_val           = cs_cacheresp_val;
+    cachereq_reg_en         = cs_cachereq_reg_en;
+    tag_array_wen           = cs_tag_array_wen;
+    tag_array_ren           = cs_tag_array_ren;
+    data_array_wen          = cs_data_array_wen;
+    data_array_ren          = cs_data_array_ren;
+    valid_bit_in            = cs_valid_bit_in;
+    valid_bits_write_en     = cs_valid_bits_write_en;
+    dirty_bit_in            = cs_dirty_bit_in;
+    dirty_bits_write_en     = cs_dirty_bits_write_en;
+    memreq_val              = cs_memreq_val;
+    memresp_rdy             = cs_memresp_rdy;
+    memresp_en              = cs_memresp_en;
+    write_data_mux_sel      = cs_write_data_mux_sel;
+    wben_mux_sel            = cs_wb_mux_sel;
+    read_data_zero_mux_sel  = cs_read_data_zero_mux_sel;
+    memreq_addr_mux_sel     = cs_memreq_addr_mux_sel;
+    read_data_reg_en        = cs_read_data_reg_en;
+    evict_addr_reg_en       = cs_evict_addr_reg_en;
+    hit_wen                 = cs_hit_wen;
+    cacheresp_type_wen      = cs_cacheresp_type_wen;
+    cacheresp_type_in       = cs_cacheresp_type;
+    memreq_type             = cs_memreq_type;
+  end
+  endtask
+
+  // Set outputs using a control signal "table"
+
+  always @(*) begin
+                                    cs( 0,    0,    0,    0,    0,    0,    0,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X );
+      case ( state_reg )
+              //                        cache cache cache tag   tag   data  data  valid valid  dirty dirty  mem    mem    mem     write   wben  read   memreq   read    evict    hit    cache     cache                               mem
+              //                        req   resp  req   array array array array bit   write  bit   write  req    resp   resp    data    sel   zero   addr     data    addr     wen    resp      resp                                req
+              //                        rdy   val   en    wen   ren   wen   ren   in    en     in    en     val    rdy    en      sel           sel    sel      en      en              type wen  type                                type
+          STATE_IDLE:               cs( 1,    0,    1,    0,    0,    0,    0,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+          STATE_TAG_CHECK:          cs( 0,    0,    0,    0,    1,    0,    0,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       0,      0,       1,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+          STATE_INIT_DATA_ACCESS:   cs( 0,    0,    0,    1,    0,    1,    0,    1,    1,     0,    1,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     1,        `VC_MEM_RESP_MSG_TYPE_WRITE_INIT,   `VC_MEM_REQ_MSG_TYPE_X);
+          STATE_READ_DATA_ACCESS:   cs( 0,    0,    0,    0,    0,    0,    1,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       1,      0,       0,     1,        `VC_MEM_RESP_MSG_TYPE_READ,         `VC_MEM_REQ_MSG_TYPE_X); // read data access does not change if data is dirty. If it was dirty, remain dirty, if it was not dirty, remain not dirty
+          STATE_WRITE_DATA_ACCESS:  cs( 0,    0,    0,    0,    0,    1,    0,    1,    1,     1,    1,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     1,        `VC_MEM_RESP_MSG_TYPE_WRITE,        `VC_MEM_REQ_MSG_TYPE_X); // write data access happens on write hit or refill + write, so always dirty
+          STATE_REFILL_REQUEST:     cs( 0,    0,    0,    0,    0,    0,    0,    0,    0,     0,    0,     1,     0,     0,      0,      0,    0,     1,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_READ);
+          STATE_REFILL_WAIT:        cs( 0,    0,    0,    0,    0,    0,    0,    0,    0,     0,    0,     0,     1,     1,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+          STATE_REFILL_UPDATE:      cs( 0,    0,    0,    1,    0,    1,    0,    1,    1,     0,    1,     0,     0,     0,      1,      1,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);  // data is always not dirty after refill
+          STATE_EVICT_PREPARE:      cs( 0,    0,    0,    0,    1,    0,    1,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       1,      1,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+          STATE_EVICT_REQUEST:      cs( 0,    0,    0,    0,    0,    0,    0,    0,    0,     0,    0,     1,     0,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_WRITE);
+          STATE_EVICT_WAIT:         cs( 0,    0,    0,    0,    0,    0,    0,    0,    0,     0,    0,     0,     1,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X );
+          STATE_WAIT:               cs( 0,    1,    0,    0,    0,    0,    0,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+          default:                  cs( 1,    0,    1,    0,    0,    0,    0,    0,    0,     0,    0,     0,     0,     0,      0,      0,    0,     0,       0,      0,       0,     0,        `VC_MEM_RESP_MSG_TYPE_X,            `VC_MEM_REQ_MSG_TYPE_X);
+      endcase
+  end
 
 endmodule
 
--- sim/lab3_mem/CacheAltDpath.v
+++ sim/lab3_mem/CacheAltDpath.v
@@ -5,9 +5,14 @@
 `ifndef LAB3_MEM_CACHE_ALT_DPATH_V
 `define LAB3_MEM_CACHE_ALT_DPATH_V
 
-//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-// LAB TASK: Include necessary files
-//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+`include "vc/mem-msgs.v"
+`include "vc/srams.v"
+`include "vc/regs.v"
+`include "vc/muxes.v"
+`include "vc/arithmetic.v"
+
+`include "lab3_mem/WbenDecoder.v"
+`include "lab3_mem/ReplUnit.v"
 
 module lab3_mem_CacheAltDpath
 #(
@@ -19,23 +24,414 @@ module lab3_mem_CacheAltDpath
 
   // Processor <-> Cache Interface
 
-  input  mem_req_4B_t   proc2cache_reqstream_msg,
-  output mem_resp_4B_t  proc2cache_respstream_msg,
+  input  mem_req_4B_t   cachereq_msg,
+  output mem_resp_4B_t  cacheresp_msg,
 
   // Cache <-> Memory Interface
 
-  output mem_req_16B_t  cache2mem_reqstream_msg,
-  input  mem_resp_16B_t cache2mem_respstream_msg,
+  output mem_req_16B_t  memreq_msg,
+  input  mem_resp_16B_t memresp_msg,
 
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // LAB TASK: Define additional ports
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // control signals (ctrl->dpath)
+
+  input  logic          cachereq_reg_en,
+  input  logic          tag_array_wen,
+  input  logic          tag_array_ren,
+  input  logic          data_array_wen,
+  input  logic          data_array_ren,
+
+  input logic           memresp_en,
+  input logic           write_data_mux_sel,
+  input logic           wben_mux_sel,
+  input logic           read_data_zero_mux_sel,
+  input logic           memreq_addr_mux_sel,
+  input logic           read_data_reg_en,
+  input logic           evict_addr_reg_en,
+  input logic [3:0]     cacheresp_type,
+  input logic           hit,
+  input logic [3:0]     memreq_type,
+
+  // status signals (dpath->ctrl)
+
+  output logic  [3:0]   cachereq_type,
+  output logic [31:0]   cachereq_addr,
+
+  output logic          tag_match
 );
 
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // LAB TASK: Implement data-path
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // Register the unpacked cachereq_msg
 
+  logic [31:0] cachereq_addr_reg_out;
+  logic [31:0] cachereq_data_reg_out;
+  logic  [3:0] cachereq_type_reg_out;
+  logic  [7:0] cachereq_opaque_reg_out;
+
+  vc_EnResetReg #(4,0) cachereq_type_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (cachereq_reg_en),
+    .d      (cachereq_msg.type_),
+    .q      (cachereq_type_reg_out)
+  );
+
+  vc_EnResetReg #(32,0) cachereq_addr_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (cachereq_reg_en),
+    .d      (cachereq_msg.addr),
+    .q      (cachereq_addr_reg_out)
+  );
+
+  vc_EnResetReg #(8,0) cachereq_opaque_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (cachereq_reg_en),
+    .d      (cachereq_msg.opaque),
+    .q      (cachereq_opaque_reg_out)
+  );
+
+  vc_EnResetReg #(32,0) cachereq_data_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (cachereq_reg_en),
+    .d      (cachereq_msg.data),
+    .q      (cachereq_data_reg_out)
+  );
+
+  assign cachereq_type = cachereq_type_reg_out;
+  assign cachereq_addr = cachereq_addr_reg_out;
+
+  // Address Mapping
+
+  logic  [1:0] cachereq_addr_byte_offset;
+  logic  [1:0] cachereq_addr_word_offset;
+  logic  [1:0] cachereq_addr_bank;
+  logic  [2:0] cachereq_addr_index;
+  logic [24:0] cachereq_addr_tag;
+
+  always @(*) begin
+    if ( p_num_banks == 1 ) begin
+      cachereq_addr_byte_offset = cachereq_addr[1:0];
+      cachereq_addr_word_offset = cachereq_addr[3:2];
+      cachereq_addr_bank        = 2'b0;
+      cachereq_addr_index       = cachereq_addr[6:4];
+      cachereq_addr_tag         = cachereq_addr[31:7];
+    end
+    else if ( p_num_banks == 4 ) begin
+      // handle address mapping for four banks
+      cachereq_addr_byte_offset = cachereq_addr[1:0];
+      cachereq_addr_word_offset = cachereq_addr[3:2];
+      cachereq_addr_bank        = cachereq_addr[5:4];
+      cachereq_addr_index       = cachereq_addr[8:6];
+      cachereq_addr_tag         = {2'b0, cachereq_addr[31:9]}; // can either pad tag with zeroes or just include bank?
+    end
+  end
+
+  // Replicate 32-bit cachereq_data to make 128-bit data word
+
+  logic [127:0] cachereq_data_replicated;
+
+  lab3_mem_ReplUnit repl_unit
+  (
+    .in_ (cachereq_data_reg_out),
+    .out (cachereq_data_replicated)
+  );
+
+  // Register memresp_msg
+
+  logic [127:0] memresp_data_reg_out;
+  logic [127:0] write_data_mux_out;
+
+  vc_EnResetReg
+  #(
+    .p_nbits(128),
+    .p_reset_value(0)
+  ) memresp_data_reg
+  (
+    .clk(clk),
+    .reset(reset),
+    .d(memresp_msg.data),
+    .q(memresp_data_reg_out),
+    .en(memresp_en)
+  );
+
+  vc_Mux2
+  #(
+    .p_nbits(128)
+  ) write_data_mux
+  (
+    .in0(cachereq_data_replicated),
+    .in1(memresp_data_reg_out),
+    .sel(write_data_mux_sel),
+    .out(write_data_mux_out)
+  );
+
+  // Write byte enable decoder
+
+  logic [15:0] wben_decoder_out;
+
+  lab3_mem_WbenDecoder wben_decoder
+  (
+    .in_ (cachereq_addr_word_offset),
+    .out (wben_decoder_out)
+  );
+
+  logic [15:0] wben_mux_out;
+
+  vc_Mux2
+  #(
+    .p_nbits(16)
+  ) write_byte_en_mux
+  (
+    .in0(wben_decoder_out),
+    .in1(16'hFFFF),
+    .sel(wben_mux_sel),
+    .out(wben_mux_out)
+  );
+
+  // Way 0 Tag array ( tags, 25 bits/tag)
+
+  logic [24:0] tag_array_read_out_way0;
+
+  vc_CombinationalBitSRAM_1rw
+  #(
+    .p_data_nbits  (25),
+    .p_num_entries (8)
+  )
+  way0_tag_array
+  (
+    .clk           (clk),
+    .reset         (reset),
+    .read_addr     (cachereq_addr_index),
+    .read_data     (tag_array_read_out_way0),
+    .write_en      (tag_array_wen),
+    .read_en       (tag_array_ren),
+    .write_addr    (cachereq_addr_index),
+    .write_data    (cachereq_addr_tag)
+  );
+
+  // Way 1 Tag array ( tags, 25 bits/tag)
+
+  logic [24:0] tag_array_read_out_way1;
+
+  vc_CombinationalBitSRAM_1rw
+  #(
+    .p_data_nbits  (25),
+    .p_num_entries (8)
+  )
+  way1_tag_array
+  (
+    .clk           (clk),
+    .reset         (reset),
+    .read_addr     (cachereq_addr_index),
+    .read_data     (tag_array_read_out_way1),
+    .write_en      (tag_array_wen),
+    .read_en       (tag_array_ren),
+    .write_addr    (cachereq_addr_index),
+    .write_data    (cachereq_addr_tag)
+  );
+
+  logic way0_tag_match;
+
+  //check if tags match in way 0 to determine if hit or miss
+  vc_EqComparator
+  #(
+    .p_nbits(25)
+  ) way0_cmp (
+    .in0(cachereq_addr_tag),
+    .in1(tag_array_read_out_way0),
+    .out(way0_tag_match)
+  );
+
+  logic way1_tag_match;
+
+  //check if tags match in way 1 to determine if hit or miss
+  vc_EqComparator
+  #(
+    .p_nbits(25)
+  ) way1_cmp (
+    .in0(cachereq_addr_tag),
+    .in1(tag_array_read_out_way1),
+    .out(way1_tag_match)
+  );
+
+  assign tag_match = {way0_tag_match , way1_tag_match};
+
+  //evicting a cache line back to main memory
+  logic [31:0] evict_addr;
+  logic [31:0] evict_addr_way0;
+  logic [31:0] evict_addr_way1;
+
+  lab3_mem_mkaddr
+  #(
+      .p_nbits(25),
+      .c_nbits(3)
+  ) addr_dataread_index_way0 (
+      .in_0(tag_array_read_out_way0),
+      .in_1(cachereq_addr_index),
+      .out_(evict_addr_way0)
+  );
+
+  lab3_mem_mkaddr
+  #(
+      .p_nbits(25),
+      .c_nbits(3)
+  ) addr_dataread_index_way1 (
+      .in_0(tag_array_read_out_way1),
+      .in_1(cachereq_addr_index),
+      .out_(evict_addr_way1)
+  );
+
+  always @(*) begin
+    if (tag_match == 2'b10) begin // if tag match in way 0
+        if (p_num_banks == 1) begin
+            evict_addr = evict_addr_way0;
+        end
+        else begin
+            evict_addr = {tag_array_read_out_way0[22:0], cachereq_addr_index, cachereq_addr_bank, 4'b0};
+        end
+    end 
+    else if (tag_match == 2'b01) begin // if tag match in way 1
+        if (p_num_banks == 1) begin
+            evict_addr = evict_addr_way1;
+        end
+        else begin
+            evict_addr = {tag_array_read_out_way1[22:0], cachereq_addr_index, cachereq_addr_bank, 4'b0};
+        end
+    end
+end
+
+  logic [31:0] evict_addr_reg_out;
+
+  vc_EnResetReg #(32,0) evict_addr_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (evict_addr_reg_en),
+    .d      (evict_addr),
+    .q      (evict_addr_reg_out)
+  );
+
+  //refill the data from memory into the correct cache line after miss event
+  logic [31:0] refill;
+
+  if (p_num_banks == 1) begin
+    lab3_mem_mkaddr //{tag, index, 4'b0000}
+    #(
+      .p_nbits(24),
+      .c_nbits(4)
+    ) addr_tag_index(
+      .in_0(cachereq_addr_tag),
+      .in_1(cachereq_addr_index),
+      .out_(refill)
+    );
+  end
+  else begin
+    assign refill = {cachereq_addr[31:4], 4'b0};
+  end
+
+  logic [31:0] memreq_addr_mux_out;
+
+  vc_Mux2
+  #(
+    .p_nbits(32)
+  ) memreq_addr_mux
+  (
+    .in0(evict_addr_reg_out),
+    .in1(refill),
+    .sel(memreq_addr_mux_sel),
+    .out(memreq_addr_mux_out)
+  );
+
+  // Way 0 Data array (16 cacheslines, 128 bits/cacheline)
+
+  logic [127:0] data_array_read_out;
+
+  vc_CombinationalSRAM_1rw #(128,16) data_array_way0
+  (
+    .clk           (clk),
+    .reset         (reset),
+    .read_addr     (cachereq_addr_index),
+    .read_data     (data_array_read_out),
+    .write_en      (data_array_wen),
+    .read_en       (data_array_ren),
+    .write_byte_en (wben_mux_out),
+    .write_addr    (cachereq_addr_index),
+    .write_data    (write_data_mux_out)
+  );
+
+  logic [127:0] read_data_zero_mux_out;
+
+  vc_Mux2
+  #(
+    .p_nbits(128)
+  ) read_data_zero_mux
+  (
+    .in0(data_array_read_out),
+    .in1(128'b0),
+    .sel(read_data_zero_mux_sel),
+    .out(read_data_zero_mux_out)
+  );
+
+  logic [127:0] read_data_reg_out;
+
+  vc_EnResetReg #(128,0) read_data_reg
+  (
+    .clk    (clk),
+    .reset  (reset),
+    .en     (read_data_reg_en),
+    .d      (read_data_zero_mux_out),
+    .q      (read_data_reg_out)
+  );
+
+  logic [31:0] cacheresp_msg_data;
+
+  vc_Mux4
+  #(
+    .p_nbits(32)
+  ) read_data_mux4 (
+    .in0(read_data_reg_out[31:0]),
+    .in1(read_data_reg_out[63:32]),
+    .in2(read_data_reg_out[95:64]),
+    .in3(read_data_reg_out[127:96]),
+    .sel(cachereq_addr_word_offset),
+    .out(cacheresp_msg_data)
+  );
+
+  assign cacheresp_msg.type_  = cacheresp_type;
+  assign cacheresp_msg.opaque = cachereq_opaque_reg_out;
+  assign cacheresp_msg.test   = {1'b0, hit};
+  assign cacheresp_msg.len    = 2'b0;
+
+  always @(*) begin
+    if (cacheresp_type == `VC_MEM_RESP_MSG_TYPE_WRITE_INIT)  cacheresp_msg.data = 0;
+    else if (cacheresp_type == `VC_MEM_RESP_MSG_TYPE_WRITE)  cacheresp_msg.data = 0;
+    else                                                      cacheresp_msg.data = cacheresp_msg_data;
+  end
+
+  assign memreq_msg.type_  = memreq_type;
+  assign memreq_msg.opaque = 8'b0;
+  assign memreq_msg.addr   = memreq_addr_mux_out;
+  assign memreq_msg.len    = 4'b0;
+  assign memreq_msg.data   = read_data_reg_out;
+
+endmodule
+
+module lab3_mem_mkaddr
+#(
+  parameter p_nbits = 1,
+  parameter c_nbits = 1
+)(
+  input logic  [p_nbits-1:0]              in_0,
+  input logic  [c_nbits-1:0]              in_1,
+  output logic [p_nbits + c_nbits +4-1:0] out_
+);
+  //addr of data to evict from cache - tag + index + 0000
+  assign out_ = { in_0, in_1, 4'b0};
 endmodule
 
 `endif
