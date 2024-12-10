61b9f6e21dfddbd5c609b08fb69378b6256bcb24
hc2263 <hc2263@cornell.edu>
2024-12-09 23:32:24 -0500

    Revert "Fix to allow display of stats for alt design"
    This reverts commit 1e34d9c8b90e15b03188b71d260d19f064a53eab.

--- sim/lab4_sys/test/harness.py
+++ sim/lab4_sys/test/harness.py
@@ -202,7 +202,7 @@ class MultiCoreTestHarness(Component):
   #-----------------------------------------------------------------------
 
   def construct( s, Sys ):
-    s.commit_inst = OutPort(4)
+    s.commit_inst = OutPort()
 
     s.srcs  = [ StreamSourceFL( Bits32, [] ) for _ in range(4) ]
     s.sinks = [ StreamSinkFL( Bits32, [] )   for _ in range(4) ]
