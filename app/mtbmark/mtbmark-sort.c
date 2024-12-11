e8a133dd065b3b45e3f2033b11265ac9d242f9cf
hc2263 <hc2263@cornell.edu>
2024-12-10 23:53:58 -0500

    Further optimise multicore sort

--- app/mtbmark/mtbmark-sort.c
+++ app/mtbmark/mtbmark-sort.c
@@ -6,10 +6,19 @@
 typedef struct
 {
   int* src;     // Pointer to the source array
-  int* dst;     // Pointer to the destination subarray
   int first;    // First index this core should process
   int last;     // (One past) last index this core should process
 } arg_t;
+
+typedef struct
+{
+  int* arr1;
+  int size1;
+  int* arr2;
+  int size2;
+  int* mergedArray;
+} arg_t_merge_two_arrays;
+
 
 // Worker function
 void work(void* arg_vptr)
@@ -19,30 +28,39 @@ void work(void* arg_vptr)
 
   // Extract fields
   int* src = arg_ptr->src;
-  int* dst = arg_ptr->dst;
   int first = arg_ptr->first;
   int last = arg_ptr->last;
 
   // Perform sorting
   ubmark_sort(src, last - first);
 
-  for (int i = 0; i < last-first; i++) {
-    dst[i] = src[i];
-  }
 }
 
 //Mergesort algorithm
 
 // Function to merge two sorted arrays
-void mergeTwoArrays(int* arr1, int size1, int* arr2, int size2, int* mergedArray) {
+void mergeTwoArrays(void* arg_vptr){
+  arg_t_merge_two_arrays* arg_ptr = (arg_t_merge_two_arrays*)arg_vptr;
+
+  int* arr1 = arg_ptr->arr1;
+  int size1 = arg_ptr->size1;
+  int* arr2 = arg_ptr->arr2;
+  int size2 = arg_ptr->size2;
+  int* mergedArray = arg_ptr->mergedArray;
+
+
   int i = 0, j = 0, k = 0;
   // Merge elements from arr1 and arr2 in sorted order
   while (i < size1 && j < size2) {
     if (arr1[i] < arr2[j]) {
-      mergedArray[k++] = arr1[i++];
+      mergedArray[k] = arr1[i];
+      i++;
+
     } else {
-      mergedArray[k++] = arr2[j++];
+      mergedArray[k] = arr2[j];
+      j++;
     }
+      k++;
   }
 
   // Add remaining elements from arr1
@@ -56,29 +74,6 @@ void mergeTwoArrays(int* arr1, int size1, int* arr2, int size2, int* mergedArray
   }
 }
 
-
-int* mergesort(int* arr0, int size0, int* arr1, int size1, int* arr2, int size2, int* arr3, int size3, int* finalSize) {
-  int* temp1 = ece4750_malloc((size0+size2)*(int)(sizeof(int)));
-  int* temp2 = ece4750_malloc((size1+size3)*(int)(sizeof(int)));
-
-  // Merge first two arrays
-  mergeTwoArrays(arr0, size0, arr2, size2, temp1);
-  // Merge last two arrays
-  mergeTwoArrays(arr1, size1, arr3, size3, temp2);
-
-  *finalSize = size0 + size1 + size2 + size3;
-  int* mergedArray = ece4750_malloc((*finalSize)*(int)(sizeof(int)));
-  mergeTwoArrays(temp1, size0 + size2, temp2, size1 + size3, mergedArray);
-
-  // Free temporary arrays
-  ece4750_free(temp1);
-  ece4750_free(temp2);
-
-
-  return mergedArray;
-}
-
-
 // Main sorting function
 void mtbmark_sort(int* x, int size)
 {
@@ -89,16 +84,10 @@ void mtbmark_sort(int* x, int size)
   int size2 = block_size;
   int size3 = size - 3*block_size;
 
-  int* arr0 = ece4750_malloc(size0*(int)(sizeof(int)));
-  int* arr1 = ece4750_malloc(size1*(int)(sizeof(int)));
-  int* arr2 = ece4750_malloc(size2*(int)(sizeof(int)));
-  int* arr3 = ece4750_malloc(size3*(int)(sizeof(int))); // Last block may be larger if size isn't divisible by 4
-
-  // Define argument for the single core
-  arg_t arg0 = {x, arr0, 0, block_size};
-  arg_t arg1 = {x+block_size, arr1, block_size, 2 * block_size};
-  arg_t arg2 = {x+2 * block_size, arr2, 2 * block_size, 3 * block_size};
-  arg_t arg3 = {x+3 * block_size, arr3, 3 * block_size, size};
+  arg_t arg0 = {x, 0, block_size};
+  arg_t arg1 = {x+block_size, block_size, 2 * block_size};
+  arg_t arg2 = {x+ 2*block_size, 2 * block_size, 3 * block_size};
+  arg_t arg3 = {x+ 3*block_size, 3 * block_size, size};
 
   // Spawn work onto core 1, 2, and 3
   ece4750_bthread_spawn(1, &work, &arg1);
@@ -113,17 +102,29 @@ void mtbmark_sort(int* x, int size)
   ece4750_bthread_join(2);
   ece4750_bthread_join(3);
 
-  int finalSize;
-  int* mergedArray = mergesort(arr0, size0,arr1, size1, arr2, size2, arr3, size3, &finalSize);
+  int* temp1 = ece4750_malloc((size0+size1)*(int)(sizeof(int)));
+  int* temp2 = ece4750_malloc((size2+size3)*(int)(sizeof(int)));
+  int* temp3 = ece4750_malloc((size)*(int)(sizeof(int)));
+
+  arg_t_merge_two_arrays arg4 = {x, size0, x+block_size, size1, temp1};
+  arg_t_merge_two_arrays arg5 = {x+ size0+size1, size2, x+ 3*block_size, size3, temp2};
+  arg_t_merge_two_arrays arg6 = {temp1, size0+size1, temp2, size2+size3, temp3};
+
+  ece4750_bthread_spawn(2, &mergeTwoArrays, &arg5);
+  ece4750_bthread_spawn(1, &mergeTwoArrays, &arg4);
+
+  ece4750_bthread_join(1);
+  ece4750_bthread_join(2);
+
+  mergeTwoArrays(&arg6);
+
 
   for (int i = 0; i < size; i++)
   {
-    x[i] = mergedArray[i];
+    x[i] = temp3[i];
   }
 
-  ece4750_free(arr0);
-  ece4750_free(arr1);
-  ece4750_free(arr2);
-  ece4750_free(arr3);
-  ece4750_free(mergedArray);
+  ece4750_free(temp1);
+  ece4750_free(temp2);
+  ece4750_free(temp3);
 }
